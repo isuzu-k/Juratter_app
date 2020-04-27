1f853ddde591e87eeec45f7bab8e4bedc32b9ecf
isuzu-k <a07ab008@yahoo.co.jp>
2020-04-27 16:00:11 +0900

    4

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,10 @@
 class PostsController < ApplicationController
   def index
+    @posts = [
+      "2020/4/27(月)自宅待機10日目@10日",
+      "2020/4/28(火)自宅待機11日目@9日",
+      "2020/4/29(水)自宅待機12日目@8日",
+      "2020/4/30(木)自宅待機13日目@7日"
+    ]
   end
 end
