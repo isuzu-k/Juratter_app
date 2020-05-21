e4aa559177a49ccee597cd462eae326c2759bcc5
isuzu-k <a07ab008@yahoo.co.jp>
2020-05-21 15:50:25 +0900

    5/21

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,40 @@
 class PostsController < ApplicationController
   def index
-    @posts = [
-      "2020/4/27(月)自宅待機10日目 @9日",
-      "2020/4/28(火)自宅待機11日目 @8日",
-      "2020/4/29(水)自宅待機12日目 @7日",
-      "2020/4/30(木)自宅待機13日目 @6日"
-    ]
+    @posts = Post.all.order(created_at: :desc)
+  end
+  def new
+    @post = Post.new
+  end
+  def create
+    @post = Post.new(content: params[:content])
+    if @post.save
+      flash[:notice] = "投稿を作成しました"
+      redirect_to("/posts/index")
+    else
+      render("posts/new")
+    end
+  end
+  def show
+    @id = params[:id]
+    @post = Post.find_by(id: params[:id])
+  end
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    if @post.save
+      flash[:notice] = "投稿を編集しました"
+      redirect_to("/posts/index")
+    else
+      render("posts/edit")
+    end
+  end
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    flash[:notice] = "投稿を削除しました"
+    redirect_to("/posts/index")
   end
 end
